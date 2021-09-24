305ec819ae53b418c9635726d5212d99d1a3e556
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2021-09-24 12:50:30 +0330

    Initial Commit

---
+++ MAPD714-F2021-Lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAPD714-F2021-Lesson2
+//
+//  Created by Ashkan Goharfar on 7/2/1400 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
