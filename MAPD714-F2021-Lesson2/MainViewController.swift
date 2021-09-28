5348d79fbc323226ac463c3585a5c33bf976a8c4
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2021-09-28 08:39:19 +0330

    All Buttons GUI have been created.

--- MAPD714-F2021-Lesson2/MainViewController.swift
+++ MAPD714-F2021-Lesson2/MainViewController.swift
@@ -11,32 +11,6 @@ class MainViewController: UIViewController {
     // variable decleration for label, ! means that the variable was initialized
     @IBOutlet weak var ResultDisplayLabel: UILabel!
     
-    
-    
-    // we use _ inside the parameter and before identifying to allow us not to include the sender parameter when we calling the function
-    // The function is event handler for button one
-//    @IBAction func ZeroButton_Pressed(_ sender: UIButton) {
-//        // ternary operator (better format)
-//        ResultDisplayLabel.text = (ResultDisplayLabel.text == "0") ? "1": "0"
-//        // write ! at the end of exrpession to force-unwrap the value
-//        print(ResultDisplayLabel.text!)
-// Simple format
-//        if(ResultDisplayLabel.text == "0"){
-//            ResultDisplayLabel.text = "1"
-//            return
-//        }
-//        self.ResultDisplayLabel.text = "0"
-        
-        // ternary operator (better format)
-//        if (self.ResultDisplayLabel.text == "Display Result!" || self.ResultDisplayLabel.text == "0"){
-//            self.ResultDisplayLabel.text = ""
-//        }
-        
-//        self.ResultDisplayLabel.text = ResultDisplayLabel.text! + "0"
-        // write ! at the end of exrpession to force-unwrap the value
-//        print(ResultDisplayLabel.text!)
-
-//    }
 
     @IBAction func ZeroButton_Pressed(_ sender: UIButton) {
         // ternary operator (better format)
